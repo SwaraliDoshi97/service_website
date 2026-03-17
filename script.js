7e331f4a13e4507a5fa317e2313d5523005c5ce7
SwaraliDoshi97 <swaralidoshi222@gmail.com>
2026-03-17 17:16:59 +0530

    changes in style and script file

--- script.js
+++ script.js
@@ -1,40 +1,30 @@
-// const toggleButtons = document.querySelectorAll(".toggle-btn");
-
-// toggleButtons.forEach(button => {
-//     button.addEventListener("click", () => {
-
-//         const card = button.parentElement;
-//         const moreContents = card.querySelectorAll(".more-content");
-
-//         moreContents.forEach(content => {
-//             if (content.style.display === "block") {
-//                 content.style.display = "none";
-//                 button.textContent = "▼ Show More";
-//             } else {
-//                 content.style.display = "block";
-//                 button.textContent = "▲ Show Less";
-//             }
-//         });
-
-//     });
-// });
 const toggleButtons = document.querySelectorAll(".toggle-btn");
 
 toggleButtons.forEach(button => {
 
-button.addEventListener("click", () => {
+    button.addEventListener("click", () => {
 
-const card = button.parentElement;
-const moreContent = card.querySelector(".more-content");
+        const card = button.parentElement;
+        const moreContent = card.querySelector(".more-content");
 
-moreContent.classList.toggle("show");
+        moreContent.classList.toggle("show");
 
-if(moreContent.classList.contains("show")){
-    button.textContent = "▲ Show Less";
-}else{
-    button.textContent = "▼ Show More";
-}
+        if (moreContent.classList.contains("show")) {
+            button.textContent = "▲ Show Less";
+        } else {
+            button.textContent = "▼ Show More";
+        }
+
+    });
 
 });
 
+window.addEventListener("scroll", () => {
+    const header = document.querySelector("header");    
+
+    if (window.scrollY > 50) {
+        header.classList.add("scrolled");
+    } else {
+        header.classList.remove("scrolled");
+    }
 });
